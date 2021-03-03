6250471dab043760661855c0166b53561fe57f62
AlbertoMartinezCicuendezURJC <a.martinezci.2020@alumnos.urjc.es>
2021-03-03 19:55:35 +0100

    03/03

--- Session-06/Seq-01.py
+++ Session-06/Seq-01.py
@@ -6,8 +6,20 @@ class Seq:
     def __str__(self): #print the object
         return self.strbases
 
+    def len(self):
+        return len(self.strbases)
+
+class Gene(Seq): #seq es el parent y Gene es el child. Gene hereda todos los methods del parent
+    pass
+
+
 s1 = Seq("AGTCGATCGATCG")
 s2 = Seq("CAGT")
+g = Gene("CGATCGACTAGCTGACTAGCTCAG")
 
-print(f"Sequence 1: {s1}")
+print(f"Sequence 1: {s1}")  #esta y la de abajo es lo mismo
+print("Sequence 1: {}".format(s1))
+print(f"  Lenght 1: {s1.len()}")
 print(f"Sequence 1: {s2}")
+print(f"  Lenght 2: {s2.len()}")
+print(f"Gene : {g}")
