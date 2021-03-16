699db415ce387a95920919a1493330b043f4b980
AlbertoMartinezCicuendezURJC <a.martinezci.2020@alumnos.urjc.es>
2021-03-16 15:07:25 +0100

    16/03

--- P1/Ex9.py
+++ P1/Ex9.py
@@ -3,14 +3,13 @@ from P0 import sequences
 
 print("-----|Practice1 ,Exercise 5|-----")
 
-
-s1 = Seq()
 GENE_FOLDER = "./sequences2/"
 gene_list = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
 
 
 for gene in gene_list:
-    s1.read_fasta(GENE_FOLDER + gene)
-    print("Gene", gene, ":", "Most frequent Base:", s1.most_repeated_base())
+    s = Seq()
+    s.read_fasta(GENE_FOLDER + gene)
+    print("Gene", gene, ":", "Most frequent Base:", s.most_repeated_base())
 
 
